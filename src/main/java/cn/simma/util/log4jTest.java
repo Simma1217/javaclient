1412855264b61b6950bd056bbb61562d39b2875a
simma-work <479921347@qq.com>
2019-01-21 14:24:48 +0800

    yyy

--- src/main/java/cn/simma/util/log4jTest.java
+++ src/main/java/cn/simma/util/log4jTest.java
@@ -1,14 +1,19 @@
 package cn.simma.util;
+
+import org.apache.log4j.Logger;
+
 /** * @author  Simma  
  *@date  创建时间：2019年1月21日 下午1:35:06
  * @version 1.0 
  * @parameter   
  */
 public class log4jTest {
-
+	private static Logger mylogger=Logger.getLogger(log4jTest.class);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		mylogger.debug("我是debug的信息");
+		mylogger.warn("我是warn的信息");
+		mylogger.info("我是info的信息");
+		mylogger.error("我是error的信息");
 	}
 
 }
